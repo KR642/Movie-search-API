77fc20ef8e4aeed548da6ec9251b4fb2f718117a
KR642 <KR642@live.mdx.ac.uk.com>
2023-12-17 17:36:02 +0000

    update search api

---
+++ vite-project/components/AddList.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+const AddList=()=>{
+
+    return (<button className='AddList'> ADD TO YOUR SHOWS</button>)
+}
+export default AddList;
--- vite-project/components/MovieSearch.jsx
+++ vite-project/components/MovieSearch.jsx
@@ -1,35 +1,48 @@
 // MovieSearch.js
 import React, { useState, useEffect } from 'react';
-
+import AddList from './AddList';
 const MovieSearch = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState(null);
 
-  useEffect(() => {
-    // Implement IMDb API integration here to fetch movie suggestions based on searchQuery
-    // You may use a library like axios for API requests
+  const fetchSuggestions = async () => {
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?t=${searchQuery}&apikey=ab3a1df`);
+      const data = await response.json();
+      setSuggestions(data); // Adjust this based on IMDb API response structure
+      console.log(data);
 
-    // Example: Fetch suggestions from IMDb API (replace with actual IMDb API endpoint)
-    const fetchSuggestions = async () => {
-      try {
-        const response = await fetch(`https://api.imdb.com/search?q=${searchQuery}`);
-        const data = await response.json();
-        setSuggestions(data.results); // Adjust this based on IMDb API response structure
-      } catch (error) {
-        console.error('Error fetching movie suggestions:', error);
-      }
-    };
+    } catch (error) {
+      console.error('Error fetching movie suggestions:', error);
+    }
+  };
 
-    // Fetch suggestions when searchQuery changes
-    fetchSuggestions();
-  }, [searchQuery]);
-
-  const handleInputChange = (e) => {
+  // useEffect(() => {
+  //   if(searchQuery!=""){
+  //     fetchSuggestions();
+  //   }else{
+  //     setSuggestions(null);
+  //   }
+  //   // Fetch suggestions when searchQuery changes
+  //   fetchSuggestions();
+  // }, [searchQuery]);
+  const handleSearch=(e)=>{
+    e.preventDefault();
+    if(searchQuery!=""){
+      fetchSuggestions();
+    }
+    else{
+      setSuggestions(null);
+    }
+  }
+  const handleInputChange = (e) => { 
     setSearchQuery(e.target.value);
   };
 
   return (
+    <div className='MovieParent'>
     <div className="SearchBar">
+      <form onSubmit={handleSearch}>
       <input
         type="text"
         value={searchQuery}
@@ -37,11 +50,29 @@ const MovieSearch = () => {
         placeholder="Search for movies..."
         className="SearchInput"
       />
-      <ul className="SearchSuggestions">
-        {suggestions.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+      </form>
+      </div>
+      {
+        suggestions==null||suggestions.Response == 'False'?<><p>No results found! Enter full title.</p></>: (
+          <div className='MovieDetails'>
+            <div className='Movie-column poster'>
+              <img src={suggestions.Poster} alt="poster" />
+            </div>
+            <div className='Movie-column details'>
+              <h1>{suggestions.Title}</h1>
+              <p>Genre: {suggestions.Genre}</p>
+              <p>Director: {suggestions.Director}</p>
+              <p>Actors: {suggestions.Actors}</p>
+              <p>Plot: {suggestions.Plot}</p>
+              <p className='MovieID'>{suggestions.imdbID}</p>
+              <AddList/>
+            </div>
+            
+          </div>
+          
+        )
+        
+      }
     </div>
   );
 };
